bae6bbf31981493ede3e9113b2f3c237e35f3661
chidam333 <chidam.sync@gmail.com>
2025-07-10 16:48:22 +0530

    azure board ss and diagrams

---
+++ 9th-July/WeatApp/Program.cs
@@ -0,0 +1,68 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+        app.MapScalarApiReference();
+
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapGet("/weather/today", () =>
+{
+    var todayWeather = new WeatherForecast
+    (
+        DateOnly.FromDateTime(DateTime.Now),
+        Random.Shared.Next(-20, 55),
+        summaries[Random.Shared.Next(summaries.Length)]
+    );
+    return todayWeather;
+})
+.WithName("GetTodayWeather");
+
+app.MapGet("/weather/summary", () =>
+{
+    return new
+    {
+        AvailableSummaries = summaries,
+        TotalCount = summaries.Length,
+        ApiVersion = "1.0"
+    };
+})
+.WithName("GetWeatherSummary");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
